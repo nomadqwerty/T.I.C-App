ab02694e1649b9990a49f606b85594f40f940822
linux <davexmike72@gmail.com>
2022-05-19 00:53:31 +1300

    rateLimit,secureHttpHeader,mongoSanitize,xss-clean,paramPollution

--- app.js
+++ app.js
@@ -1,52 +1,76 @@
 // 3rd party modules
 const express = require('express');
 const morgan = require('morgan');
-
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xssClean = require('xss-clean');
+const hpp = require('hpp');
 // AppError class:A_E
-const AppError = require('./utils/AppError')
+const AppError = require('./utils/AppError');
 // error contoller function
-const globalErrorHandler = require('./controllers/errorController')
+const globalErrorHandler = require('./controllers/errorController');
 
 // require user and service Routes modules
 const serviceRouter = require('./routes/serviceRoutes');
-const trainingRouter = require('./routes/trainingRoutes')
-const departmentRouter = require('./routes/departmentRoutes')
-const conferenceRouter = require('./routes/conferenceRoutes')
+const trainingRouter = require('./routes/trainingRoutes');
+const departmentRouter = require('./routes/departmentRoutes');
+const conferenceRouter = require('./routes/conferenceRoutes');
 const userRouter = require('./routes/userRoutes');
 // instance of the express module
 const app = express();
 
+// GLOBAL MIDDLEWARE
+// secure http headers with helmet
+app.use(helmet());
 
-if(process.env.NODE_ENV==='developement') {
+// logger middlerware
+if (process.env.NODE_ENV === 'developement') {
   // 3rd party middleware:logger
-  app.use(morgan('dev'))
+  app.use(morgan('dev'));
 }
-  ;
-// express middleware
-app.use(express.json());
-// midlewar for req time
+// express middleware:body parser read data from request
+app.use(express.json({ limit: '25kb' }));
+// midleware for req time
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-   next();
+  next();
 });
+// ratelimiting for bruteforce&dos: ratelimit options
+const options = {
+  max: 500,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests!, please retry in 1 hour',
+};
+const limit = rateLimit(options);
+app.use('/api', limit);
 
-
+// mongo sanitize
+app.use(mongoSanitize());
+// xss-clean
+app.use(xssClean());
+// parameter pollution
+const list = ['duration', 'duration', 'name'];
+const paramProtection = hpp({
+  whitelist: list,
+});
+app.use(paramProtection);
 // /////////////////////////////////////////////////////
 // mount router with middleware
+
+// /////
 app.use('/api/v1/services/', serviceRouter);
-app.use('/api/v1/trainings/',trainingRouter)
-app.use('/api/v1/departments/',departmentRouter)
-app.use('/api/v1/conferences/',conferenceRouter)
+app.use('/api/v1/trainings/', trainingRouter);
+app.use('/api/v1/departments/', departmentRouter);
+app.use('/api/v1/conferences/', conferenceRouter);
 app.use('/api/v1/user/', userRouter);
 
 // handle unhandled routes: 404 not found
-app.all('*',(req,res,next)=>{
-  
-  next(new AppError(`${req.originalUrl} is undefined`,404))
-
-})
+app.all('*', (req, res, next) => {
+  next(new AppError(`${req.originalUrl} is undefined`, 404));
+});
 
 ////////////////////////////////////////////////////////Global error middleware handler
-app.use(globalErrorHandler)
-// ////////////////////////////////////////////////////export the express app to the server 
-module.exports = app
+app.use(globalErrorHandler);
+// ////////////////////////////////////////////////////export the express app to the server
+module.exports = app;
--- utils/ApiFeatures.js
+++ utils/ApiFeatures.js
@@ -1,55 +1,55 @@
 // API features
-class ApiFeatures{
-    constructor(query,queryStr){
-      this.query = query
-      this.queryStr = queryStr
+class ApiFeatures {
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
+  filter() {
+    const queryObj = { ...this.queryStr };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => {
+      delete queryObj[el];
+    });
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    queryStr = JSON.parse(queryStr);
+
+    this.query = this.query.find(queryStr);
+
+    return this;
+  }
+  sort() {
+    if (this.queryStr.sort) {
+      // sort multiple fields
+      let sortBy = this.queryStr.sort;
+      sortBy = sortBy.replace(/,/g, ' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
     }
-    filter(){
-      const queryObj = {...this.queryStr}
-      const excludedFields = ['page','sort','limit','fields']  
-      excludedFields.forEach(el=>{ delete queryObj[el]})
-  
-      let queryStr = JSON.stringify(queryObj)
-      queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g,match=>`$${match}`)
-      queryStr = JSON.parse(queryStr)
-      
-      this.query = this.query.find(queryStr)
-  
-      return this
+    return this;
+  }
+  limitFields() {
+    if (this.queryStr.fields) {
+      let fields = this.queryStr.fields;
+      fields = fields.replace(/,/g, ' ');
+      this.query = this.query.select(fields);
+    } else {
+      // exclude fields
+      this.query = this.query.select('-__v');
     }
-    sort(){
-      if(this.queryStr.sort){
-        // sort multiple fields
-        let sortBy = this.queryStr.sort
-        sortBy = sortBy.replace(/,/g,' ')
-        this.query = this.query.sort(sortBy)
-      }else{
-        this.query = this.query.sort('-createdAt')
-      } 
-      return this
-    }
-    limitFields(){
-      if(this.queryStr.fields){
-        let fields = this.queryStr.fields
-        fields = fields.replace(/,/g,' ')
-        this.query = this.query.select(fields)
-      }else{
-        // exclude fields
-        this.query = this.query.select('-__v')
-      }
-      return this
-    }
-    pagination(){
-      const page = Number(this.queryStr.page)||1
-      const limit = Number(this.queryStr.limit)||10
-      const skip = (page-1)*limit
-  
-      this.query = this.query.skip(skip).limit(limit)
-      
-      return this
-  
-    }
+    return this;
+  }
+  pagination() {
+    const page = Number(this.queryStr.page) || 1;
+    const limit = Number(this.queryStr.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
 }
 
-module.exports = ApiFeatures
-  
+module.exports = ApiFeatures;
