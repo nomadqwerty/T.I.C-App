0c74094dd8222da0c00aa14ebf4824c308c81dae
linux <davexmike72@gmail.com>
2022-06-10 01:01:24 +1300

    conf detail page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -4,9 +4,45 @@ const express = require('express');
 const AppError = require('../utils/AppError');
 const Testimonie = require('../models/testimonieModel');
 const Service = require('../models/serviceModel');
+const Conference = require('../models/conferenceModel');
 
 const catchAsync = require('../utils/catchAsync');
 
+exports.getRoot = catchAsync(async (req, res, next) => {
+  const resources = [
+    {
+      name: 'Impact Conferences',
+      imageCover: 'tour-2-cover.jpg',
+      summary: `join Impact Conferences and see the manifestation of God in your life`,
+      location: 'Port-Harcourt, Rivers State',
+      path: 'overview1',
+    },
+    {
+      name: 'services',
+      imageCover: 'tour-2-cover.jpg',
+      summary: `join Impact Services and see the manifestation of God in your life`,
+      location: 'Port-Harcourt, Rivers State',
+      path: 'overview',
+    },
+    {
+      name: 'Impact Trainings',
+      imageCover: 'tour-2-cover.jpg',
+      summary: `join Impact Training and see the manifestation of God in your life`,
+      location: 'Port-Harcourt, Rivers State',
+    },
+    {
+      name: 'Impact Testimonies',
+      imageCover: 'tour-2-cover.jpg',
+      summary: `Impact Testimonies, see the manifestation of God in the life of other`,
+      location: 'International',
+    },
+  ];
+
+  res.status(200).render('root', {
+    title: 'resources',
+    resources,
+  });
+});
 exports.getOverview = catchAsync(async (req, res, next) => {
   const services = await Service.find();
   res.status(200).render('overview', {
@@ -14,13 +50,33 @@ exports.getOverview = catchAsync(async (req, res, next) => {
     services,
   });
 });
+// conference overview
+exports.getOverviewConference = catchAsync(async (req, res, next) => {
+  const conferences = await Conference.find();
+  console.log(conferences[0]);
+  res.status(200).render('overview1', {
+    title: 'All conferences',
+    conferences,
+  });
+});
 
 exports.getservice = catchAsync(async (req, res, next) => {
-  const service = await Service.findOne({ name: req.params.name });
-
+  console.log(req.params);
+  const service = await Service.findOne({ slug: req.params.slug });
+  console.log(service);
   // pass into
   res.status(200).render('service', {
     title: service.name,
     service,
+  });
+});
+exports.getconference = catchAsync(async (req, res, next) => {
+  console.log(req.params);
+  const conference = await Conference.findOne({ slug: req.params.slug });
+  console.log(conference);
+  // pass into
+  res.status(200).render('conference', {
+    title: conference.name,
+    conference,
   });
 });
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,11 +3,14 @@ const viewController = require('../controllers/viewController');
 const viewRouter = express.Router();
 
 // render overview
-viewRouter.get('/', viewController.getOverview);
+viewRouter.get('/overview', viewController.getOverview);
+viewRouter.get('/overview1', viewController.getOverviewConference);
+viewRouter.get('/', viewController.getRoot);
 
 // render single tour
 // viewRouter.get('/service', viewController.getservice);
 
-viewRouter.route('/services/:name').get(viewController.getservice);
+viewRouter.route('/services/:slug').get(viewController.getservice);
+viewRouter.route('/conferences/:slug').get(viewController.getconference);
 
 module.exports = viewRouter;
